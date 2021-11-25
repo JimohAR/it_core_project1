0f45347732a6aa827d00dec905ebdeed97bb4eb5
JimohAR <abolarinwarildwan@gmail.com>
2021-11-25 22:37:02 +0000

    coding the overview analysis function

--- scripts/task1.py
+++ scripts/task1.py
@@ -1,9 +1,20 @@
+from tabulate import tabulate
 class task1:
-    def __init__(self):
+    def __init__(self, data):
+        self.data = data 
         pass 
 
     def user_overview_analysis(self):
-        pass
+        ## stripping out the column names for easy parsing
+        new_data_col = [i.strip() for i in list(self.data.columns)]
+        self.data.columns = new_data_col
+
+        handset_data = self.data[['Handset Manufacturer', 'Handset Type',]]
+        top10_handet = handset_data["Handset Type"][handset_data["Handset Type"] != "undefined"].value_counts()
+        print(tabulate(top10_handet.to_frame(), tablefmt= "grid"))
+        
+
+
 
     def get_aggregate_data(self):
         pass
