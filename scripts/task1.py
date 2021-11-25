63127e9ac7a8eb43652d18f8140bc1bcd9e46e8b
JimohAR <abolarinwarildwan@gmail.com>
2021-11-25 21:50:37 +0000

    made the schema for the task1 class script

---
+++ scripts/task1.py
@@ -0,0 +1,12 @@
+class task1:
+    def __init__(self):
+        pass 
+
+    def user_overview_analysis(self):
+        pass
+
+    def get_aggregate_data(self):
+        pass
+
+    def analyse_aggregates(self):
+        pass
