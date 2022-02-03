dca2b1877f32747a052ef769f6c07e836613284a
JimohAR <abolarinwarildwan@gmail.com>
2022-02-03 09:26:44 +0100

    made little changes to the utils.py file

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -1,18 +1,22 @@
 import os
 import gdown 
-import streamlit as st
 
-@st.cache
-def get_gdrive_file():
-    id = "1LorBoIoTw1dKgPiUo9-1AfYZmU9UUBFQ"
-    file_path = set_path() + "user_engagement_data.csv"
+
+def get_gdrive_files():
+    path = set_path()
+    ids = ["1YaYSDui8cefj0aYmlY_i6eKz-q0fnNbN", "1LorBoIoTw1dKgPiUo9-1AfYZmU9UUBFQ"]
+    file_paths = [path + "telco.csv", path + "user_engagement_data.csv"]
     try:    
-        set_path().split('it_core_project1')[1]
+        path.split('it_core_project1')[1]
     except IndexError:
         # on remote (heroku/github)
-        # need to download the data for analysis
-        gdown.download(id=id, output=file_path, quiet=False)
+        # need to download the some data for analysis
+        if not (os.path.isfile(path + "telco.csv")):
+            gdown.download(id=ids[0], output=file_paths[0], quiet=False)
+        if not (os.path.isfile(path + "user_engagement_data.csv")):
+            gdown.download(id=ids[1], output=file_paths[1], quiet=False)
 
+            
 
 def set_path(folder="data"):
     if ("data" in os.listdir()) or ("src" in os.listdir()):
